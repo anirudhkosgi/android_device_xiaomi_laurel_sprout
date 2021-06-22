8cdf7d1b18e6e4ceaba80824be99dbfa63313e77
Lunarixus <Nathanb644@hotmail.co.uk>
2021-06-22 12:18:04 +0530

    laurel_sprout: Remove packages at build time
    Signed-off-by: Anirudhkosgi <kosgi34@gmail.com>

--- device.mk
+++ device.mk
@@ -405,6 +405,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
