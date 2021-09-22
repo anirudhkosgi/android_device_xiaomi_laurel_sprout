47eca7b19c6c133e3427b875f6597eb990d42199
Magicxavi <rolfpeterkesche@gmail.com>
2021-09-22 03:53:56 +0300

    laurel_sprout: copy custom apns-conf
    E/TelephonyProvider(2846): FileNotFoundException for /system/etc/apns-conf.xml:java.io.FileNotFoundException: /system/etc/apns-conf.xml: open failed: ENOENT (No such file or directory)

--- device.mk
+++ device.mk
@@ -110,6 +110,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+
 # ATRACE_HAL
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
