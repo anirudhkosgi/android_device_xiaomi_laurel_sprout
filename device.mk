9fc27040c7eb832994bc44434f23d710992eef82
ujjwalkirorimal <ujjwalkirorimal@gmail.com>
2021-10-13 20:50:01 +0530

    laurel_sprout: Update DRM/Clearkey HIDL to v1.4

--- device.mk
+++ device.mk
@@ -259,8 +259,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
