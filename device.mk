3526128847f98323df7d460b4168b47a5cf3b791
weivincewang <wvw@google.com>
2022-04-14 19:46:39 +0300

    laurel_sprout: Improve boot time and pull in bootanim display time
    1) launch bootanim early
    2) parallel slow init operations: write sysfs
    
    After CL:
    Boot time saved 330ms, bootanim triggered early before data/ mounted
    
    Bug: 36780513
    Test: laurel boots
    Change-Id: I02803179746710413f4b1e2372f3550cd95d1581

--- device.mk
+++ device.mk
@@ -322,11 +322,16 @@ $(foreach target, $(shell cat $(LOCAL_PATH)/vndk/vndk.txt), $(eval PRODUCT_PACKA
 
 PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
 
+# Insmod files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init.insmod.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/init.insmod.cfg
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.class_main.sh \
     init.msm.usb.configfs.rc \
+    init.insmod.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
