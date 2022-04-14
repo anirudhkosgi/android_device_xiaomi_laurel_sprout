3c19cb671dd82828026cfce8a959e1cc1226b9e2
Magicxavi <rolfpeterkesche@gmail.com>
2022-04-14 23:26:56 +0300

    laurel_sprout: ship gallery go
    Because aosp one have issues

--- syberia_laurel_sprout.mk
+++ syberia_laurel_sprout.mk
@@ -41,7 +41,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := google/redfin/redfin:12/SP1A.211105.003/7757856:user/release-keys
 
 PRODUCT_PACKAGES += \
-    RemovePackages
+    RemovePackages \
+    GalleryGo2
 #    GalleryGo2
 #    FirefoxLite \
 #    GCamGo
