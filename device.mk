466fd3a39e472e46f407fb0e93a335b932bf75e5
anirudhkosgi <kosgi34@gmail.com>
2022-05-26 09:21:31 +0200

    laurel_sprout: define hals path

--- device.mk
+++ device.mk
@@ -457,3 +457,9 @@ $(call inherit-product-if-exists, vendor/xiaomi/laurel_sprout/laurel_sprout-vend
 #Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/lahaina-yupikqrd-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/lahaina-yupikqrd-snd-card_Button_Jack.kl
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    hardware/qcom/display \
+    hardware/qcom/media
