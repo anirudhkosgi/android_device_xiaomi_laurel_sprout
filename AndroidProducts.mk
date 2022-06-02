42ad717d7920776fcf3ef4b8735a9272807dd8cf
Magicxavi <rolfpeterkesche@gmail.com>
2022-06-03 00:03:29 +0300

    aospa

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/syberia_laurel_sprout.mk
+    $(LOCAL_DIR)/aospa_laurel_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    syberia_laurel_sprout-user \
-    syberia_laurel_sprout-userdebug \
-    syberia_laurel_sprout-eng
+    aospa_laurel_sprout-user \
+    aospa_laurel_sprout-userdebug \
+    aospa_laurel_sprout-eng
--- aospa_laurel_sprout.mk
+++ aospa_laurel_sprout.mk
@@ -19,8 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Syberia stuff
-$(call inherit-product, vendor/syberia/common.mk)
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
 
 # Inherit from laurel_sprout device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,7 +28,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := laurel_sprout
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := syberia_laurel_sprout
+PRODUCT_NAME := aospa_laurel_sprout
 PRODUCT_MODEL := Mi A3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -46,14 +46,12 @@ PRODUCT_PACKAGES += \
 #    FirefoxLite \
 #    GCamGo
     
-# Syberia
-SYBERIA_BUILD_TYPE := OFFICIAL
+# Aospa
 TARGET_BOOT_ANIMATION_RES := 1080
 FORCE_OTA := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_USES_BLUR := true
 EXTRA_UDFPS_ANIMATIONS := true
-WITH_GMS=true
 TARGET_GAPPS_ARCH := arm64
 
 # Use gestures by default
