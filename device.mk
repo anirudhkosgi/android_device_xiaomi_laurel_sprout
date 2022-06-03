ee9f827397d9774eda7c2972aac37f8a06982dd8
Magicxavi <rolfpeterkesche@gmail.com>
2022-06-04 01:36:57 +0300

    laurel_sprout: drop prebuilt libhidl
    Already shipped with source

--- device.mk
+++ device.mk
@@ -284,12 +284,6 @@ ifneq ($(AB_OTA_UPDATER),true)
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery
 endif
-
-# HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0
   
 # HIDL VNDK
 $(foreach target, $(shell cat $(LOCAL_PATH)/configs/vndk/vndk.txt), $(eval PRODUCT_PACKAGES += $(target).vendor))
