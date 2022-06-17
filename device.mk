ec2b1079e571e3de26dfe75058f3c2a05aebd65d
Magicxavi <rolfpeterkesche@gmail.com>
2022-06-17 05:39:26 +0300

    laurel_sprout: drop some prebuilt camera blobs and build their OSS variants

--- device.mk
+++ device.mk
@@ -185,13 +185,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hidl.memory.block@1.0.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libstdc++.vendor \
     libcamera2ndk_vendor \
-    libdng_sdk.vendor \
-    Snap
+    libdng_sdk.vendor
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.expose.aux=1 \
