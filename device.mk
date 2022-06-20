a1d8a8738575f54df99505fe2b26a634619db2e6
anirudhkosgi <kosgi34@gmail.com>
2022-06-20 17:54:06 +0200

    Revert "laurel_sprout: don't use power qti for now"
    This reverts commit 1a983fe193bfc516e048d1bfdf60e11313b62698.

--- device.mk
+++ device.mk
@@ -395,7 +395,9 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.laurel_sprout-libperfmgr \
-    android.hardware.power.stats-service.laurel_sprout
+    android.hardware.power.stats-service.laurel_sprout \
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
