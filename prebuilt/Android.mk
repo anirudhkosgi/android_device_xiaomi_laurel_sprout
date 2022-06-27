84b4ce49951e5c67739cf90f578bb29a73ed24bc
Magicxavi <rolfpeterkesche@gmail.com>
2022-06-27 23:55:11 +0300

    laurel_sprout: block GCamGO from aospa
    We use miui camera

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -15,7 +15,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX Snap Gmail2 Maps talkback YouTube
+LOCAL_OVERRIDES_PACKAGES := AudioFX Snap Gmail2 Maps talkback YouTube GoogleCameraGo
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
