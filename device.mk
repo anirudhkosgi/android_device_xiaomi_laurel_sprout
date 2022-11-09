ff79a1c3095bfbbea964c18edec752a4c7d95053
anurdhkosgi <kosgi34@gmail.com>
2022-11-09 17:53:06 +0000

    Revert "laurel_sprout: add offline charging animation from pixel"
    This reverts commit b87c958902bbc3c75806f2d8dacef3a4df2ab341.

--- device.mk
+++ device.mk
@@ -342,11 +342,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
-# Offline charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    product_charger_res_images
-
 # OMX
 PRODUCT_PACKAGES += \
     libavservices_minijail \
